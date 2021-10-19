8fd3c5b91a70e9bf6e0857ca9f9c87e941caf876
Dmitry Boltovsky <demetron11@gmail.com>
2021-10-19 19:37:29 +0300

    Initial commit

---
+++ src/main/java/com/example/ex1/Ex1Application.java
@@ -0,0 +1,14 @@
+package com.example.ex1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+@SpringBootApplication
+public class Ex1Application extends SpringBootServletInitializer {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Ex1Application.class, args);
+    }
+
+}
